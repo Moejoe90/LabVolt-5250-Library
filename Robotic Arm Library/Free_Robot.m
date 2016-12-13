b5292166b12806b3bc1856b2bd64829024d38c46
Moejoe90 <mohammed.elsharif@gmail.com>
2016-12-13 15:48:37 +0100

    Free Robot Servo Motors

--- Robotic Arm Library/Free_Robot.m
+++ Robotic Arm Library/Free_Robot.m
@@ -1,17 +1,12 @@
-function Free_Robot(obj)
+function Free_Robot(obj) %Free the Robot Arm Servo Motors
 
-if(Check_Robot_State(obj) == 0)
-
-return;
-
+if(Check_Robot_State(obj) == 0) %Check if Robot Arm is connected and initialized 
+return; %Return error
 end
 
-fprintf(obj,'free');
-
-while(obj.BytesAvailable == 0)
-
+fprintf(obj,'free'); %Free the Robot Motors
+while(obj.BytesAvailable == 0) %Wait for Robot To respond
 end
+result = fscanf(obj,'%c',obj.BytesAvailable) % Print out Robots Response, add ; to get rid of screen reporting
 
-result = fscanf(obj,'%c',obj.BytesAvailable)
-
-end
+end %Function End
